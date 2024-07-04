029287e52fa4f633717874c46312b2b823d8a711
Julio Cesar Napoles Puente <juliocesarnapolespuente@gmail.com>
2024-07-04 13:37:28 +0200

    Add StartCanvas to all page

--- src/App.jsx
+++ src/App.jsx
@@ -19,20 +19,16 @@ const App = () => {
   useFavicon();
   return (
     <BrowserRouter>
-      <div className='relative z-0 bg-primary'>
-        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
-          <Navbar />
-          <Hero />
-        </div>
+      <div className='relative z-0 bg-primary'>        
+        <Navbar />
+        <Hero />
         <About />
         <Experience />
         <Tech />
         <Works />
-        <Feedbacks />
-        <div className='relative z-0'>
-          <Contact />
-          <StarsCanvas />
-        </div>
+        <Feedbacks />       
+        <Contact />     
+        <StarsCanvas />
       </div>
     </BrowserRouter>
   );
