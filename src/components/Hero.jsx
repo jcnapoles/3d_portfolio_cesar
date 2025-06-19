4980f89a7ce7a4ef5c7f72171b28a1742d72648b
Julio Cesar Napoles Puente <juliocesarnapolespuente@gmail.com>
2025-06-19 17:25:08 +0200

    Change description

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -22,8 +22,8 @@ const Hero = () => {
               Hi, I'm <span className='text-[#915EFF]'>Cesar</span>
             </h1>
             <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-              I develop web applications, user <br className='sm:block hidden' />
-              interfaces and microservices.
+              Strategic tech leader driving innovation <br className='sm:block hidden' />
+              and growth for multiple startups.
             </p>   
            
           </div>
